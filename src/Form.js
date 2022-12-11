dee04fb138fc30063329549959f2dc83646f4749
mariia-popova <110813375+mariia-popova@users.noreply.github.com>
2022-12-11 11:27:49 +0100

    Changed functions to arrow functions.

--- src/Form.js
+++ src/Form.js
@@ -9,52 +9,52 @@ export default function Form(props) {
     setInputValue(event.target.value);
   }
 
-  function handleSubmit(event) {
+  const handleSubmit = async (event) => {
     event.preventDefault();
+
     let url = `https://api.shecodes.io/weather/v1/current?query=${inputValue}&key=aed6a8e29c1edbe73t3fba79abf409do&units=metric`;
-    axios.get(url).then(saveWeatherResault);
+    const weatherResponse = await axios.get(url);
+    props.setWeather(weatherResponse.data);
 
     let apiForecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${inputValue}&key=aed6a8e29c1edbe73t3fba79abf409do&units=metric`;
-    axios.get(apiForecastUrl).then(saveForecastResault);
-  }
-
-  function saveWeatherResault(response) {
-    props.setWeather(response.data);
-  }
-
-  function saveForecastResault(response) {
-    const formatedArray = response.data.daily.slice(1, 6);
+    const forecastResponse = await axios.get(apiForecastUrl);
+    const formatedArray = forecastResponse.data.daily.slice(1, 6);
     props.setForecast(formatedArray);
-  }
+  };
 
   function getCurrentWeather(event) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(fetchCurrentWeather);
   }
 
-  function fetchCurrentWeather(position) {
+  const fetchCurrentWeather = async (position) => {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
-    axios
-      .get(
-        `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=aed6a8e29c1edbe73t3fba79abf409do`
-      )
-      .then(saveWeatherResault);
+    const responseCurrentWeather = await axios.get(
+      `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=aed6a8e29c1edbe73t3fba79abf409do`
+    );
+    props.setWeather(responseCurrentWeather.data);
 
-    axios
-      .get(
-        `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=aed6a8e29c1edbe73t3fba79abf409do`
-      )
-      .then(saveForecastResault);
-  }
+    const responseForecast = await axios.get(
+      `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=aed6a8e29c1edbe73t3fba79abf409do`
+    );
+    const formatedArray = responseForecast.data.daily.slice(1, 6);
+    props.setForecast(formatedArray);
+  };
 
   useEffect(() => {
-    let url = `https://api.shecodes.io/weather/v1/current?query=Kyiv&key=aed6a8e29c1edbe73t3fba79abf409do&units=metric`;
-    axios.get(url).then(saveWeatherResault);
+    const fetchInitialData = async () => {
+      let url = `https://api.shecodes.io/weather/v1/current?query=Kyiv&key=aed6a8e29c1edbe73t3fba79abf409do&units=metric`;
+      const weatherResponse = await axios.get(url);
+      props.setWeather(weatherResponse.data);
 
-    let apiForecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=Kyiv&key=aed6a8e29c1edbe73t3fba79abf409do&units=metric`;
-    axios.get(apiForecastUrl).then(saveForecastResault);
-  }, [saveWeatherResault, saveForecastResault]);
+      let apiForecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=Kyiv&key=aed6a8e29c1edbe73t3fba79abf409do&units=metric`;
+      const forecastResponse = await axios.get(apiForecastUrl);
+      const formatedArray = forecastResponse.data.daily.slice(1, 6);
+      props.setForecast(formatedArray);
+    };
+    fetchInitialData();
+  }, [props.setWeather, props.setForecast]);
 
   return (
     <form className="Form" onSubmit={handleSubmit}>
